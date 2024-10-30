77a337bf8fba191f678644257c7b1de57afe4b6a
Parth Kulkarni <parthkulkarni15@gmail.com>
2024-10-30 18:13:42 -0400

    added student data file

---
+++ user_populate_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO user (user_id, email, first_name, last_name, password, role_no)
+VALUES
+('ErPe1024', 'ez356@example.mail', 'Eric', 'Perrig', 'qwdmq', 3),
+('AlAr1024', 'aa23@edu.mail', 'Alice', 'Artho', 'omdsws', 3),
+('BoTe1024', 'bt163@template.mail', 'Bob', 'Temple', 'sak+=', 3),
+('LiGa1024', 'li123@example.edu', 'Lily', 'Gaddy', 'cnaos', 3),
+('ArMo1024', 'am213@example.edu', 'Aria', 'Morrow', 'jwocals', 3),
+('KeRh1014', 'kr21@example.edu', 'Kellan', 'Rhodes', 'camome', 3),
+('SiHa1024', 'sh13@example.edu', 'Sienna', 'Hayes', 'asdqm', 3),
+('FiWi1024', 'fw23@example.edu', 'Finn', 'Wilder', 'f13mas', 3),
+('LeMe1024', 'lm56@example.edu', 'Leona', 'Mercer', 'ca32', 3);
+
+INSERT INTO student (stud_id)
+VALUES 
+('ErPe1024'),
+('AlAr1024'),
+('BoTe1024'),
+('LiGa1024'),
+('ArMo1024'),
+('KeRh1014'),
+('SiHa1024'),
+('FiWi1024'),
+('LeMe1024');
+
