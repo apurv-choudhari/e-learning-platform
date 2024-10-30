a9caf3d835b4d251107e7c0d11e1f8689bbd92d3
apurv-choudhari <apurv.choudhari1@gmail.com>
2024-10-30 17:12:24 -0400

    add screen for query execution

--- connect.py
+++ connect.py
@@ -3,6 +3,7 @@ from mysql.connector import errorcode
 from dotenv import load_dotenv
 import manageDB as mdb
 import os
+import subprocess
 
 load_dotenv()
 
@@ -12,35 +13,59 @@ host='classdb2.csc.ncsu.edu'
 database=os.getenv('DBMS_USER')
 print(f"User:{user}, Password:{password}, Host:{host}, Database:{database}")
 
-try:
-    print(os.getenv('DBMS_USER'))
-    reservationConnection = mysql.connector.connect(
-        user=user,
-        password=password, 
-        host=host,
-        database=database)
-    print("Connect Successful.\n")
-    cursor = reservationConnection.cursor()
+def setupDB():
+    try:
+        print(os.getenv('DBMS_USER'))
+        reservationConnection = mysql.connector.connect(
+            user=user,
+            password=password,
+            host=host,
+            database=database)
+        print("Connect Successful.\n")
+        cursor = reservationConnection.cursor()
 
-    if mdb.clearAll(cursor):
-        print("Database Cleared.")
+        if mdb.clearAll(cursor):
+            print("Database Cleared.")
 
-    if mdb.createDB(cursor):
-        print("Database Creation Successful.")
-    else:
-        print("Database Creation Failed.")
-    
-    if mdb.populateDB(cursor):
-        print("Database Population Successful.")
-    else:
-        print("Database Population Failed.")
+        if mdb.createDB(cursor):
+            print("Database Creation Successful.")
+        else:
+            print("Database Creation Failed.")
+        
+        if mdb.populateDB(cursor):
+            print("Database Population Successful.")
+        else:
+            print("Database Population Failed.")
 
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR: 
-        print('Invalid credentials')
-    elif err.errno == errorcode.ER_BAD_DB_ERROR: 
-        print('Database not found')
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('Invalid credentials')
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print('Database not found')
+        else:
+            print('Cannot connect to database:',err)
     else:
-        print('Cannot connect to database:',err)
-else:
-    reservationConnection.close()
+        reservationConnection.close()
+
+import argparse
+
+def main():
+    setupDB()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'choice',
+        choices=['app', 'queries']
+    )
+
+    args = parser.parse_args()
+
+    if args.choice == 'app':
+        print(f'Starting ZyBooks.')
+        subprocess.run(["python3", "app.py"])
+    elif args.choice == 'queries':
+        print("Execute Sample Queries.")
+        subprocess.run(["python3", "execute_sample_queries.py"])
+
+
+if __name__ == "__main__":
+    main()
