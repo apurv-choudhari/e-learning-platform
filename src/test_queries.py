161b3c2b0e47c4e802b17eb81c3895ff36ffc688
apurv-choudhari <apurv.choudhari1@gmail.com>
2024-11-06 12:42:46 -0500

    update test queries

--- src/test_queries.py
+++ src/test_queries.py
@@ -1,7 +1,7 @@
 from mysql.connector import Error
 from flow.admin.admin_db_utils import connectDB
 
-def execute_query(query_num):
+def execute_query(query_num, param_list):
     conn, cursor = connectDB()
     if conn is None or cursor is None:
         print("Database connection failed.")
@@ -10,7 +10,7 @@ def execute_query(query_num):
     # TODO:
     # Write queries here
     queries = {
-        1: "SELECT * FROM user;",
+        1: "select count(section_id) from section where chapter_id = 'chap01' and textbook_id = %s;",
         2: "SELECT * FROM textbook;",
         3: "SELECT * FROM course;",
         4: "SELECT * FROM faculty;",
@@ -22,7 +22,7 @@ def execute_query(query_num):
     try:
         query = queries.get(query_num)
         if query:
-            cursor.execute(query)
+            cursor.execute(query, param_list)
             if query_num in {1, 2, 3, 4, 5, 6, 7}:
                 results = cursor.fetchall()
                 for row in results:
@@ -37,10 +37,25 @@ def execute_query(query_num):
         cursor.close()
         conn.close()
 
+
+def params_for_query_1():
+    tid = input("Provide Textbook-id: ")
+    return [tid]
+
+def no_params():
+    return []
+
+get_query_params = {
+    "1": params_for_query_1,
+    # "2": params_for_query_2,
+    # "3": params_for_query_3,
+    # "4": params_for_query_4
+}
+
 def test_user_queries():
     while True:
         print("\n--- Query Executor ---")
-        print("1. Execute Query 1")
+        print("1. Query 1: Write a query that prints the number of sections of the first chapter of a textbook.")
         print("2. Execute Query 2")
         print("3. Execute Query 3")
         print("4. Execute Query 4")
@@ -52,7 +67,8 @@ def test_user_queries():
         choice = input("Enter your choice (1-8): ")
 
         if choice in [str(i) for i in range(1, 8)]:
-            execute_query(int(choice))
+            param_list = get_query_params[choice]()
+            execute_query(int(choice), param_list)
         elif choice == "8":
             print("Returning to Main Menu...")
             break
